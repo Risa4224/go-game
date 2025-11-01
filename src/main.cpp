1c34fbd5cde74a18a6e64a49243b5c1bbcbee325
ntbaohevhev <gfdg6651@gmail.com>
2025-11-01 11:06:05 +0700

    add base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include "board.h"
+
 using namespace std;
 
-int main() {
-    cout << "Hello, Go Game!" << endl;
+int main(){
+    Board b;
+    b.placePiece(0, 0, BLACK);
+    b.placePiece(1, 1, WHITE);
+    b.printSelf();
     return 0;
 }
