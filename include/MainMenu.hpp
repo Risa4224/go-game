e6f77ffe263b261913eda309d2b9a940c64ca32b
Risa <dttoan2007@gmail.com>
2025-11-16 15:31:31 +0700

    Added Play , Exit , Settings to MainMenu

--- include/MainMenu.hpp
+++ include/MainMenu.hpp
@@ -12,6 +12,20 @@ class MainMenu : public Engine::State
 private :
     std::shared_ptr<Context> m_context;
     sf::Text m_gameTitle;
+    sf::Text m_playButton;
+    sf::Text m_exitButton;
+    sf::Text m_settingsButton;
+
+    bool m_isPlayButtonSelected;
+    bool m_isPlayButtonPressed;
+
+    bool m_isExitButtonSelected;
+    bool m_isExitButtonPressed;
+
+    bool m_isSettingButtonSelected;
+    bool m_isSettingButtonPressed;
+
+
 public:
     MainMenu(std::shared_ptr<Context>& context);
     ~MainMenu();
--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -2,7 +2,16 @@
 #include <SFML/Window/Event.hpp>
 MainMenu::MainMenu(std::shared_ptr<Context> &context)
     :m_context(context),
-    m_gameTitle(m_context->m_assets->GetFont(MAIN_FONT))
+    m_exitButton(m_context->m_assets->GetFont(MAIN_FONT),"Exit",40),
+    m_playButton(m_context->m_assets->GetFont(MAIN_FONT),"Play",40),
+    m_settingsButton(m_context->m_assets->GetFont(MAIN_FONT),"Settings",40),
+    m_gameTitle(m_context->m_assets->GetFont(MAIN_FONT),"Go Game",60),
+    m_isPlayButtonSelected(true),
+    m_isExitButtonSelected(false),
+    m_isPlayButtonPressed(false),
+    m_isExitButtonPressed(false),
+    m_isSettingButtonPressed(false),
+    m_isSettingButtonSelected(false)
 {
 }
 
@@ -12,8 +21,26 @@ MainMenu::~MainMenu() {
 
 void MainMenu::Init()
 {
-    m_gameTitle.setString("Go Game");
-    m_gameTitle.setPosition({320,320});
+    //title
+    m_gameTitle.setOrigin(m_gameTitle.getLocalBounds().getCenter());
+    m_gameTitle.setPosition({m_context->m_window->getSize().x/2,m_context->m_window->getSize().y/2-100});
+
+    //play button
+
+    m_playButton.setOrigin(m_playButton.getLocalBounds().getCenter());
+    m_playButton.setPosition({m_context->m_window->getSize().x/2,m_context->m_window->getSize().y/2-20});
+
+    //setting button
+
+
+    m_settingsButton.setOrigin(m_settingsButton.getLocalBounds().getCenter());
+    m_settingsButton.setPosition({m_context->m_window->getSize().x/2,m_context->m_window->getSize().y/2+60});
+
+    //exit button
+
+    m_exitButton.setOrigin(m_exitButton.getLocalBounds().getCenter());
+    m_exitButton.setPosition({m_context->m_window->getSize().x/2,m_context->m_window->getSize().y/2+140});
+
 }
 void MainMenu::ProcessInput()
 {
@@ -23,16 +50,92 @@ void MainMenu::ProcessInput()
         {
             m_context->m_window->close();
         }
+        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+        {
+            if(keyPressed->scancode==sf::Keyboard::Scancode::Up)
+            {
+                if(!m_isPlayButtonSelected)
+                {
+                    if(m_isExitButtonSelected)
+                    {
+                        m_isExitButtonSelected=false;
+                        m_isSettingButtonSelected=true;
+                    }
+                    else
+                    {
+                        m_isSettingButtonSelected=false;
+                        m_isPlayButtonSelected=true;
+                    }
+                }
+            }
+            if(keyPressed->scancode==sf::Keyboard::Scancode::Down)
+            {
+                if(!m_isExitButtonSelected)
+                {
+                    if(m_isPlayButtonSelected)
+                    {
+                        m_isPlayButtonSelected=false;
+                        m_isSettingButtonSelected=true;
+                    }
+                    else
+                    {
+                        m_isSettingButtonSelected=false;
+                        m_isExitButtonSelected=true;
+                    }
+                }
+            }
+            if(keyPressed->scancode==sf::Keyboard::Scancode::Enter)
+            {
+                m_isExitButtonPressed=false;
+                m_isPlayButtonPressed=false;
+                m_isSettingButtonPressed=false;
+                if(m_isPlayButtonSelected) m_isPlayButtonPressed=true;
+                if(m_isExitButtonSelected) m_isExitButtonPressed=true;
+                if(m_isSettingButtonSelected) m_isSettingButtonPressed=true;
+            }
+        }
     }
 }
 void MainMenu::Update(sf::Time deltaTime)
 {
-
+    if(m_isPlayButtonSelected)
+    {
+        m_playButton.setFillColor(sf::Color::Yellow);
+        m_exitButton.setFillColor(sf::Color::White);
+        m_settingsButton.setFillColor(sf::Color::White);
+    }
+    else if(m_isExitButtonSelected)
+    {
+        m_exitButton.setFillColor(sf::Color::Yellow);
+        m_playButton.setFillColor(sf::Color::White);
+        m_settingsButton.setFillColor(sf::Color::White);
+    }
+    else
+    {
+        m_settingsButton.setFillColor(sf::Color::Yellow);
+        m_exitButton.setFillColor(sf::Color::White);
+        m_playButton.setFillColor(sf::Color::White);
+    }
+    if(m_isPlayButtonPressed)
+    {
+        //Todo GO to Play State
+    }
+    else if(m_isSettingButtonPressed)
+    {
+        //TOdo go to settings state
+    }
+    else if(m_isExitButtonPressed)
+    {
+        m_context->m_window->close();
+    }
 }
 void MainMenu::Draw()
 {
     m_context->m_window->clear();   
     m_context->m_window->draw(m_gameTitle);
+    m_context->m_window->draw(m_exitButton);
+    m_context->m_window->draw(m_playButton);
+    m_context->m_window->draw(m_settingsButton);
     m_context->m_window->display();
 
 }
